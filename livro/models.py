5a4f85df6d7eff54a0dbf930122ea33321a0e3d0
josuekla <josueklayslerjk@gmail.com>
2024-09-10 16:47:16 -0300

    Dando os primeiros passos para a construção da biblioteca usando django

---
+++ livro/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import date
+
+class Livros(models.Model):
+    nome = models.CharField(max_length=100)
+    autor = models.CharField(max_length=30)
+    co_autor = models.CharField(max_length=30, blank=True)
+    data_cadastro = models.DateField(default = date.today)
+    emprestrado = models.BooleanField(default=False)
+    nome_emprestado = models.CharField(max_length=30, blank=True) #Aqui é o nome da pessoa que para quem o livro foi emprestado
+    data_emprestimo = models.DateField(blank=True) # O dia que o livro foi emprestado
+    tempo_duração = models.DateField(blank=True)
+
+    class Meta:
+        verbose_name = 'livro'
+    
